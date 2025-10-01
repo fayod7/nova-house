e8116d0b705547adce90224196609832aab04b66
fayod7 <fayodnv@gmail.com>
2025-10-01 16:23:28 +0500

    i18n

--- src/layout/components/subheader/Subheader.tsx
+++ src/layout/components/subheader/Subheader.tsx
@@ -1,15 +1,22 @@
 import { Instagram } from 'lucide-react';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Subheader = () => {
-    
+    const { i18n } = useTranslation()
   return (
     <div className='w-full text-white bg-black py-2 duration-300'>
         <div className='container flex items-center justify-between'>
            <div className='flex items-center gap-2'>
-             <button className='font-semibold cursor-pointer'>UZ</button>
-            <button className='font-semibold cursor-pointer'>EN</button>
-            <button className='font-semibold cursor-pointer'>RU</button>
+             <button 
+             onClick={() => i18n.changeLanguage('uz')}
+             className='font-semibold cursor-pointer' >UZ</button>
+            <button 
+            onClick={() => i18n.changeLanguage('en')}
+            className='font-semibold cursor-pointer'>EN</button>
+            <button 
+            onClick={() => i18n.changeLanguage('ru')}
+            className='font-semibold cursor-pointer'>RU</button>
            </div>
             <a href='#' className='flex items-center gap-1'>
                 <Instagram />
