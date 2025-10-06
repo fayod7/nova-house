0bfe83bdd2815b5228774399f01b7234b7f6a98b
fayod7 <fayodnv@gmail.com>
2025-10-06 19:09:48 +0500

    ..

--- src/features/collections/components/CollectionsViewSkeleton.tsx
+++ src/features/collections/components/CollectionsViewSkeleton.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 
 const CollectionsViewSkeleton = () => {
   return (
-    <div className="container grid grid-cols-3 gap-3 pb-1 max-[990px]:grid-cols-2 max-[470px]:grid-cols-1">
+    <div className="container grid grid-cols-3 gap-3 pb-1 max-[990px]:grid-cols-2">
       {Array(6).fill(null).map((_, inx) => (
         <div key={inx} className="flex flex-col gap-2">
           <div className="w-full h-[300px] animation rounded-md"></div>
