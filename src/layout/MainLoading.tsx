1ad9251b081b205ba084902a8f6d964f2b1065c1
fayod7 <fayodnv@gmail.com>
2025-10-08 19:16:30 +0500

    search

--- src/layout/MainLoading.tsx
+++ src/layout/MainLoading.tsx
@@ -4,8 +4,8 @@ const MainLoading = () => {
   return (
        <div className="flex justify-center items-center h-screen bg-[#1d1d1d]">
       <div className="flex flex-col items-center space-y-2">
-        <div className='flex gap-5 items-center'>
-            <img className='animate-spin size-[70px]' src={logo} alt="" />
+        <div className='flex flex-col gap-2 items-center'>
+            <img className='w-[140px] h-[120px]' src={logo} alt="" />
            <h2 className='text-[#c9c9c9] text-3xl font-light font-serif tracking-4 uppercase'>Nova house</h2>
         </div>
       </div>
