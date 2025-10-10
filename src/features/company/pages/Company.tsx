b14aca5eb57c49db6417125b5850d51d20296926
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-10-10 19:08:46 +0500

    salom

--- src/features/company/pages/Company.tsx
+++ src/features/company/pages/Company.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 import.meta.env.VITE_API_BASE_URL;
 const Company = () => {
-  const {t} = useTranslation()
+  const { t } = useTranslation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -62,17 +62,24 @@ const Company = () => {
             <div className="p-6 bg-[#f4f1ee] rounded-2xl hover:shadow-lg transition">
               <Users className="w-10 h-10 mx-auto text-[#404040] mb-4" />
               <h3 className="font-semibold mb-2">{t("company.skilled")}</h3>
-              <p className="text-sm text-gray-600">{t("company.skilled_desc")}</p>
+              <p className="text-sm text-gray-600">
+                {t("company.skilled_desc")}
+              </p>
             </div>
             <div className="p-6 bg-[#f4f1ee] rounded-2xl hover:shadow-lg transition">
               <Award className="w-10 h-10 mx-auto text-[#404040] mb-4" />
-              <h3 className="font-semibold mb-2">{ t("company.brand")}</h3>
-              <p className="text-sm text-gray-600">
-                { t("company.brand_desc")}
-              </p>
+              <h3 className="font-semibold mb-2">{t("company.brand")}</h3>
+              <p className="text-sm text-gray-600">{t("company.brand_desc")}</p>
             </div>
           </div>
         </div>
+      </div>
+      <div className="container ">
+        <iframe
+          className="w-full h-[450px] border-0 rounded-2xl my-20"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.1234567890123!2d69.29600000000000!3d41.38311400000000!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef32709e6c6bb%3A0x1c14ce22472b80b3!2sYunusobad+Medical+Center!5e0!3m2!1sen!2suz!4v1620000000000!5m2!1sen!2suz"
+          loading="lazy"
+        ></iframe>
       </div>
     </section>
   );
